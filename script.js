2f1eed9489962e4c7dcf67690ed33c0bd3136f7c
Buligin98 <buligindaniil98@gmail.com>
2022-12-19 18:50:30 +0400

    HW1

---
+++ script.js
@@ -0,0 +1,15 @@
+const name = "Daniil";
+
+const surname = "Buligini";
+
+const male = "Male";
+
+const dateOfBirth = 1998;
+
+(100 - (10 + 15) / 5) * 3;
+
+// answer = 9
+
+let example = 101 % 2;
+
+console.log(example);
